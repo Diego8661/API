263fada4f5d75f49250c7fabaa4d4081f6e8fa09
Diego Barboza <diegobarboza.medeiros@gmail.com>
2023-12-06 16:23:17 -0300

    API pronta

---
+++ src/mocks/users.js
@@ -0,0 +1,14 @@
+module.exports = [
+    {
+        id: 1,
+        name: 'Diego',
+    },
+    {
+        id: 2,
+        name: 'João',
+    },
+    {
+        id: 3,
+        name: 'Maria',
+    },
+]
